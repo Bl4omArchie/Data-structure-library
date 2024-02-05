97d155b38a51028e900b95179c98652cec2d95fe
archie bloom <saitama33000@protonmail.com>
2024-02-05 16:34:42 +0100

    experimentation for a small benchmark framework

--- includes/benchmark.h
+++ includes/benchmark.h
@@ -7,6 +7,18 @@
 #include <time.h>
 
 
-void file_list(uint64_t n);
+// I'm trying to make a small framework for benchmarking
+typedef void (*bench_func_t)(int argc, char *argv[]);
 
-#endif
+struct Benchmark {
+    double recorded_time;
+    bench_func_t func;
+};
+
+void add_benchmark(struct Benchmark *evaluation, bench_func_t *bench_to_insert);
+void start_benchmark(struct Benchmark *evaluation, int argc, char *argv[]);
+void benchmark_insert(uint64_t num_element);
+void benchmark_remove(DoublyLinkedList *to_remove);
+void benchmark_lookup_element(DoublyLinkedList *to_find);
+
+#endif  
