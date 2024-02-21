a2afb47dc55706de3e91f662a6a94e429fa9b1a7
Bl4omArchie <grlch.alexandre@proton.me>
2024-02-21 12:12:25 +0100

    bench and test correction

--- bench.c
+++ bench.c
@@ -4,15 +4,16 @@
 
 
 
-int main() {
+int main(int argc, char *argv[]) {
     srand((unsigned int)time(NULL));
 
     DLL myList;
     init_linked_list(&myList);
     set_data(&myList, generateRandom64BitNumber());
 
-
-    //benchmark(&insert_data_tail, 1000, 2, &myList, generateRandom64BitNumber());
+    argc = 2;
+    argv[argc] = &myList, generateRandom64BitNumber();
+    benchmark(&insert_data_tail, 1000, argc, &argv);
 
     return 1;
 }
--- main.c
+++ main.c
@@ -5,16 +5,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-uint64_t bigEndianToLittleEndian(uint64_t bigEndianNumber) {
-    return ((bigEndianNumber & 0x00000000000000FFULL) << 56) |
-           ((bigEndianNumber & 0x000000000000FF00ULL) << 40) |
-           ((bigEndianNumber & 0x0000000000FF0000ULL) << 24) |
-           ((bigEndianNumber & 0x00000000FF000000ULL) << 8)  |
-           ((bigEndianNumber & 0x000000FF00000000ULL) >> 8)  |
-           ((bigEndianNumber & 0x0000FF0000000000ULL) >> 24) |
-           ((bigEndianNumber & 0x00FF000000000000ULL) >> 40) |
-           ((bigEndianNumber & 0xFF00000000000000ULL) >> 56);
-}
+
 
 int time_test() {
     uint64_t a = 0b1111111101101011111111111111111111111111111111111111111111110000;
@@ -24,32 +15,16 @@ int time_test() {
 
     gettimeofday(&tv_start, NULL);
 
+    //function to test
     printf ("%d\n", predict_carry(a, b));
 
     gettimeofday(&tv_end, NULL);
     double mtime = (tv_end.tv_sec - tv_start.tv_sec) * 1000000.0 + (tv_end.tv_usec - tv_start.tv_usec) / 1000000.0; // in ms
     printf ("%f\n", mtime);
-
-    
-    gettimeofday(&tv_start, NULL);
-
-    printf ("%d\n", predict_carry_v2(a, b));
-
-    gettimeofday(&tv_end, NULL);
-    mtime = (tv_end.tv_sec - tv_start.tv_sec) * 1000000.0 + (tv_end.tv_usec - tv_start.tv_usec) / 1000000.0; // in ms
-    printf ("%f\n", mtime);
-
-    gettimeofday(&tv_start, NULL);
-    printf ("%d\n", predict_carry_v3(a, b));
-    gettimeofday(&tv_end, NULL);
-    mtime = (tv_end.tv_sec - tv_start.tv_sec) * 1000000.0 + (tv_end.tv_usec - tv_start.tv_usec) / 1000000.0; // in ms
-    printf ("%f\n", mtime);
 }
 
-int main() {
-    time_test();
 
-
+void add_tail_test() {
     DLL myList, start, end;
     init_linked_list(&myList);
     init_linked_list(&start);
@@ -67,9 +42,12 @@ int main() {
     insert_element_tail(&myList, &start);
     
     
-
     add_tail_range(&myList, start, end);
     display_linked_list(myList);
+}
+
+int main() {
+
 
     return 1;
 }
