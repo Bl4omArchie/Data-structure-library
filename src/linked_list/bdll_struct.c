d8056a20db96901152c97da0a50ae61ce81b7bc1
Bl4omArchie <grlch.alexandre@proton.me>
2024-08-26 17:46:12 +0200

    semantic for BDLL

--- src/linked_list/bdll_struct.c
+++ src/linked_list/bdll_struct.c
@@ -15,7 +15,7 @@ void bdll_set_value(bdll_node *node, uint64_t value) {
 }
 
 
-int bdll_insert_branch(bdll_node *node, uint64_t value) {
+int bdll_insert_value_branch(bdll_node *node, uint64_t value) {
     if (node == NULL)
         return -1;
 
@@ -31,34 +31,87 @@ int bdll_insert_branch(bdll_node *node, uint64_t value) {
         newNode->tail = node->tail;
     
     node->branch = newNode;
-
     return 1;
 }
 
 int bdll_insert_value_head(bdll_node *node, uint64_t value) {
+    if (node == NULL) 
+        return -1;
+
+    bdll_node *newNode = (bdll_node*)malloc(sizeof(bdll_node));
+    bdll_init(newNode);
+    bdll_set_value(newNode, value);
+    
+    if (node->head != NULL) {
+        node->head->tail = newNode;
+        newNode->head = node->head;
+    }
+
+    node->head = newNode;
+    newNode->tail = node;
     return 1;
 }
 
 int bdll_insert_value_tail(bdll_node *node, uint64_t value) {
+    if (node == NULL) 
+        return -1;
+
+    bdll_node *newNode = (bdll_node*)malloc(sizeof(bdll_node));
+    bdll_init(newNode);
+    bdll_set_value(newNode, value);
+
+    if (node->tail != NULL) {
+        node->tail->head = newNode;
+        newNode->tail = node->tail;
+    }
+
+    node->tail = newNode;
+    newNode->head = node;
     return 1;
 }
 
 
 int bdll_insert_node_branch(bdll_node *node, bdll_node *to_replace) {
+
     return 1;
 }
 
 int bdll_insert_node_head(bdll_node *node, bdll_node *to_replace) {
+    if (node == NULL || to_replace == NULL)
+        return -1;
+
+    if (to_replace->head != NULL)
+        to_replace->head->tail = to_replace->tail;
+    if (to_replace->tail != NULL)
+        to_replace->tail->head = to_replace->head;
+
+    if (node->head != NULL) {
+        node->head->tail = to_replace;
+        to_replace->head = node->head;
+    } 
+    
+    node->head = to_replace;
+    to_replace->tail = node;
     return 1;
 }
 
 int bdll_insert_node_tail(bdll_node *node, bdll_node *to_replace) {
-    return 1;
-}
+    if (node == NULL || to_replace == NULL)
+        return -1;
 
+    if (to_replace->head != NULL)
+        to_replace->head->tail = to_replace->tail;
+    if (to_replace->tail != NULL)
+        to_replace->tail->head = to_replace->head;
 
-int bdll_remove_node(bdll_node *node) {
-    return 1;
+    if (node->tail != NULL) {
+        node->tail->head = to_replace;
+        to_replace->tail = node->tail;
+    }
+    
+    node->tail = to_replace;
+    to_replace->head = node;
+    return 1; 
 }
 
 int bdll_remove_branch(bdll_node *node) {
@@ -96,6 +149,23 @@ int bdll_free_head(bdll_node *node) {
 }
 
 int bdll_free_tail(bdll_node *node) {
+    return 1;
+}
+
+int bdll_remove_node(bdll_node *node) {
+    if (node == NULL)
+        return -1;
+
+    if (node->head != NULL)
+        node->head->tail = NULL;
+
+    if (node->tail != NULL)
+        node->tail->head = NULL;
+
+    if (node->branch != NULL) 
+        bdll_free(node->branch);
+
+    free(node);
     return 1;
 }
 
